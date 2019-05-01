5a905318cf74e3c9552cab90991a8fb7a13936d6
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-04-30 22:25:22 -0400

    functional in-stock/out-of-stock response with inquirer

---
+++ bamazonCustomer.js
@@ -0,0 +1,75 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer
+    .prompt({
+        name: "buyerAction",
+        type: "list",
+        message: "Would you like to [BUY] and item or [EXIT]?",
+        choices: ["BUY", "EXIT"]
+    })
+    .then(function(answer){
+        if(answer.buyerAction === "BUY"){
+            purchase();
+        } else {
+            connection.end();
+        }
+    });
+}
+
+function purchase(){
+    connection.query("SELECT * FROM items", function(err, results){
+        if (err) throw err;
+
+        inquirer
+          .prompt([
+              {
+                  name: "choice",
+                  type: "rawlist",
+                  choices: function(){
+                      var choiceArray = [];
+                      for (var i= 0; i < results.length; i++) {
+                          choiceArray.push(results[i].product_name);
+                      }
+                      return choiceArray;
+                  },
+                  message: "What item would you like to purchase?"
+              },
+              {
+                  name: "quantity",
+                  type: "input",
+                  message: "How many would you like to purchase?"
+              }
+          ])
+          .then(function(answer){
+              var chosenItem;
+              for (var i = 0; i < results.length; i++) {
+                  if (results[i].product_name === answer.choice) {
+                      chosenItem = results[i];
+                  }
+              }
+              if(chosenItem.stock_quantity >= parseInt(answer.quantity)) {
+                  console.log("Quantity avalible for purchase")
+              } else {
+                  console.log("Insufficent quantity")
+              }
+          })
+    })
+}
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE items(
     id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100),
     department_name VARCHAR(30),
-    price DECIMAL(10,4),
+    price DECIMAL(10,2),
     stock_quantity INT(50),
     PRIMARY KEY (id)
 );

a2cf87e893b663619c3f97725f90e19e746feee7
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-04-30 21:37:31 -0400

    initial db setup

---
+++ bamazon_db.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE items(
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100),
+    department_name VARCHAR(30),
+    price DECIMAL(10,4),
+    stock_quantity INT(50),
+    PRIMARY KEY (id)
+);
